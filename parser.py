ee399d5e9d333b83950121573c23d37c33ab022a
mehdimj <mehdi.m.jamai@gmail.com>
2015-09-28 16:16:43 -0400

    Parsing SGM by BS4 Tokenizing with NLTK

--- parser.py
+++ parser.py
@@ -1,10 +1,20 @@
 __author__ = 'Mehdi'
 
+# tokenize
+# normalize =
+    # case fold
+    # remove stop words
+    # stemming
+# make an inverted index
 
-import untangle
-obj = untangle.parse('reuters21578.tar/reut2-000.sgm')
+from bs4 import BeautifulSoup
+import nltk
+# nltk.download('punkt')
 
+doc = open("reuters21578.tar/reut2-000.sgm")
+soup = BeautifulSoup(doc, "html.parser")
 
-
-
-
+for body in soup.find_all('body'):
+    textOnly = body.get_text()
+    tokens = nltk.word_tokenize(textOnly)
+    print tokens
