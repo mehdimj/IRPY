47fbca1ed8f0ccdccf2fe08792f7c016016678b6
mehdimj <mehdi.m.jamai@gmail.com>
2015-10-13 23:02:43 -0400

    stop words, lower case

--- parser.py
+++ parser.py
@@ -9,12 +9,26 @@ __author__ = 'Mehdi'
 
 from bs4 import BeautifulSoup
 import nltk
-# nltk.download('punkt')
+from nltk.corpus import stopwords
 
+
+# nltk.download('punkt') due to errors
+
+# Getting Stop Words
+stops = set(stopwords.words('english'))
+# Getting the collection
 doc = open("reuters21578.tar/reut2-000.sgm")
+# Parsing the collection
 soup = BeautifulSoup(doc, "html.parser")
 
+# Getting body text, tokenizing, lower case, and stopwords removal
 for body in soup.find_all('body'):
     textOnly = body.get_text()
     tokens = nltk.word_tokenize(textOnly)
-    print tokens
+    words = [w.lower() for w in tokens]
+for w in words:
+    if w not in stops:
+        count = 0
+        print w
+        ++count
+print count
